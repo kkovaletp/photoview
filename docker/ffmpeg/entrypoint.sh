cccc271c73d028790f8c47e1e8cb75b1c9681253
Konstantin Koval <kkb@ukr.net>
2024-06-21 17:38:14 +0300

    switch from pip to pipenv

--- docker/ffmpeg/entrypoint.sh
+++ docker/ffmpeg/entrypoint.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
+set -e
 
-source /opt/venv/bin/activate
-gunicorn -b 0.0.0.0:"${PHOTOVIEW_FFMPEG_PORT}" \
+pipenv run gunicorn \
+  -b 0.0.0.0:"${PHOTOVIEW_FFMPEG_PORT}" \
   --timeout "${PHOTOVIEW_FFMPEG_TIMEOUT}" \
   --log-level "${PHOTOVIEW_FFMPEG_LOGLEVEL}" \
   --worker-class gevent \
