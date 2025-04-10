1c789bfb1bef9f76321a29527981efd9dc194dde
Konstantin Koval <kkb@ukr.net>
2025-04-10 20:20:45 +0300

    Try to fix AlbumPage test

--- ui/src/Pages/AlbumPage/AlbumPage.test.tsx
+++ ui/src/Pages/AlbumPage/AlbumPage.test.tsx
@@ -4,12 +4,87 @@ import React from 'react'
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import AlbumPage from './AlbumPage'
 import { renderWithProviders } from '../../helpers/testUtils'
+import { gql } from '@apollo/client'
 
 vi.mock('../../hooks/useScrollPagination')
 
-test('AlbumPage renders', () => { //TODO: fix long output and "No more mocked responses for the query" error
+// Define the album query based on the error message
+const ALBUM_QUERY = gql`
+  fragment MediaGalleryFields on Media {
+    id
+    type
+    blurhash
+    thumbnail {
+      url
+      width
+      height
+    }
+    highRes {
+      url
+    }
+    videoWeb {
+      url
+    }
+    favorite
+  }
+
+  fragment AlbumGalleryFields on Album {
+    id
+    title
+    subAlbums(order: {order_by: "title", order_direction: $orderDirection}) {
+      id
+      title
+      thumbnail {
+        id
+        thumbnail {
+          url
+        }
+      }
+    }
+    media(
+      paginate: {limit: $limit, offset: $offset}
+      order: {order_by: $mediaOrderBy, order_direction: $orderDirection}
+      onlyFavorites: $onlyFavorites
+    ) {
+      ...MediaGalleryFields
+    }
+  }
+
+  query albumQuery($id: ID!, $onlyFavorites: Boolean, $mediaOrderBy: String, $orderDirection: OrderDirection, $limit: Int, $offset: Int) {
+    album(id: $id) {
+      ...AlbumGalleryFields
+    }
+  }
+`;
+
+// Create a mock with the expected structure
+const mockAlbumQuery = {
+  request: {
+    query: ALBUM_QUERY,
+    variables: {
+      id: "1",
+      onlyFavorites: false,
+      mediaOrderBy: "date_shot",
+      orderDirection: "ASC",
+      offset: 0,
+      limit: 200
+    }
+  },
+  result: {
+    data: {
+      album: {
+        id: "1",
+        title: "Test Album",
+        subAlbums: [],
+        media: []
+      }
+    }
+  }
+};
+
+test('AlbumPage renders', () => {
   renderWithProviders(<AlbumPage />, {
-    mocks: [],
+    mocks: [mockAlbumQuery],
     initialEntries: ['/album/1'],
     path: "/album/:id",
     route: <AlbumPage />
@@ -17,6 +92,4 @@ test('AlbumPage renders', () => { //TODO: fix long output and "No more mocked re
 
   expect(screen.getByText('Sort')).toBeInTheDocument()
   expect(screen.getByLabelText('Sort direction')).toBeInTheDocument()
-
-  screen.debug()
 })
