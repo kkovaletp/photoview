fae5b3b4694d44932468af22a90ad6785041c8c7
Konstantin Koval <kkb@ukr.net>
2025-06-02 22:42:19 +0300

    install ca-certs in prepare

--- dependencies/prepare.sh
+++ dependencies/prepare.sh
@@ -20,6 +20,7 @@ dpkg --add-architecture "$DEBIAN_ARCH"
 apt-get update
 apt-get install -y --no-install-recommends \
   curl \
+  ca-certificates \
   crossbuild-essential-"${DEBIAN_ARCH}" \
   libc-dev:"${DEBIAN_ARCH}" \
   autoconf \
