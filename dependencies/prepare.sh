72dbcd2b3cc80c0d68cb4e6a77b9f53f0ecae938
Konstantin Koval <kkb@ukr.net>
2025-06-04 18:39:05 +0300

    Fix incorrect quoting

--- dependencies/prepare.sh
+++ dependencies/prepare.sh
@@ -31,6 +31,7 @@ apt-get install -y --no-install-recommends \
   cmake
 
 dpkg-architecture -a "$DEBIAN_ARCH" >/env
-export "$(cat /env)"
+# shellcheck disable=SC2046
+export $(cat /env)
 echo PKG_CONFIG_PATH=/usr/lib/"${DEB_HOST_MULTIARCH}"/pkgconfig >>/env
 cat /env
