5fc3b69883433ee4c70c6a999659742ccc9014c5
Konstantin Koval <kkb@ukr.net>
2025-08-02 17:24:03 +0300

    try fs-capture-search

--- scripts/coverity-capture-api.sh
+++ scripts/coverity-capture-api.sh
@@ -3,15 +3,13 @@ set -euo pipefail
 
 export PATH="/app/cov-analysis/bin:${PATH}"
 
-set
-which go
-go version
-
 cov-configure --comptype go --compiler "$(which go)"
+cat /app/cov-analysis/config/coverity_config.xml
+
 coverity capture \
-    --dir /app/cov-int --language go --project-dir /app/api \
+    --dir /app/cov-int --project-dir /app/api \
     --file-exclude-regex ".*_test\.go|.*/go/pkg/.*" \
-    -- go build -v -o photoview . \
+    --fs-capture-search /app/api \
   || (\
     cat /app/cov-int/build-log.txt && \
     exit 1 \
