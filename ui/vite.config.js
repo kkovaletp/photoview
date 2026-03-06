afcc62730577953e20ab1baae82c4026f5e4e20a
Konstantin Koval <kkb@ukr.net>
2026-03-06 14:21:18 +0200

    Remove unneeded default from vite config

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -56,7 +56,6 @@ export default defineConfig(async ({ command, mode }) => {
         junit: './junit-report.xml',
       },
       coverage: {
-        provider: 'v8',
         reporter: ['text', 'lcov', 'json', 'html'],
         reportOnFailure: true,
       },
