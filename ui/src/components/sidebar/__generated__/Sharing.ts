f21b13ef189846d6ff7f3f58f11d2a9f62a48b15
Konstantin Koval <kkb@ukr.net>
2026-04-18 14:57:24 +0300

    Remove unused generated types

--- ui/src/components/sidebar/__generated__/Sharing.ts
+++ ui/src/components/sidebar/__generated__/Sharing.ts
@@ -1,24 +1,5 @@
 import * as Types from '../../../__generated__/globalTypes'
 
-export type SidebarGetPhotoSharesQueryVariables = Types.Exact<{
-  id: Types.Scalars['ID']['input']
-}>
-
-export type SidebarGetPhotoSharesQuery = {
-  __typename?: 'Query'
-  media: {
-    __typename?: 'Media'
-    id: string
-    shares: Array<{
-      __typename?: 'ShareToken'
-      id: string
-      token: string
-      hasPassword: boolean
-      expire?: any | null
-    }>
-  }
-}
-
 export type SidebarGetAlbumSharesQueryVariables = Types.Exact<{
   id: Types.Scalars['ID']['input']
 }>
--- ui/src/components/timelineGallery/__generated__/TimelineGallery.test.ts
+++
@@ -1,8 +0,0 @@
-import * as Types from '../../../__generated__/globalTypes'
-
-export type EarliestMediaQueryVariables = Types.Exact<{ [key: string]: never }>
-
-export type EarliestMediaQuery = {
-  __typename?: 'Query'
-  myMedia: Array<{ __typename?: 'Media'; id: string; date: any }>
-}
