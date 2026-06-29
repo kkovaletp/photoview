ee7baf2ad45515ad6f578da22739764c5d5e302a
Konstantin Koval <kkb@ukr.net>
2026-06-29 11:44:36 +0300

    TODO for button styles

--- ui/src/Pages/SettingsPage/ScannerSection.tsx
+++ ui/src/Pages/SettingsPage/ScannerSection.tsx
@@ -90,6 +90,7 @@ const ScannerSection = () => {
     inProgressLabel.length,
     defaultLabel.length
   ) + 2
+  //TODO: Fix the button styles for the disabled states
   const stableButtonStyle = { minWidth: `${longestCh}ch` }
 
   const handleStart = () => {
